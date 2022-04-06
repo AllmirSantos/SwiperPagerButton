bdd56f0e7e62c0c83c8ac4264f5758cfea5a8c2d
Almir Stark <Almirwebdev@gmail.com>
2022-04-05 22:18:34 -0300

    Swiper Pager Button

--- App.js
+++ App.js
@@ -1,112 +1,144 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
 
-import React from 'react';
-import type {Node} from 'react';
+import React, { useRef, useState } from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
+    Animated,
+    Dimensions,
+    ScrollView,
+    StyleSheet,
+    Text,
+    TouchableOpacity,
+    View
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const { width } = Dimensions.get('window');
 
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
-
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+export default function SwiperPagerButton() {
+    const scrollX = useRef(new Animated.Value(0)).current;
+    const buttons = ['Tela 1', 'Tela 2', 'Tela 3'];
+    const onCLick = i => this.scrollView.scrollTo({ x: i * width });
+    return (
+        <View style={styles.container}>
+            <View style={{ padding: 5, paddingTop: 0 }}>
+                <ButtonContainer buttons={buttons} onClick={onCLick} scrollX={scrollX} />
+            </View>
+            <ScrollView
+                ref={e => (this.scrollView = e)}
+                horizontal
+                pagingEnabled
+                decelerationRate="fast"
+                showsHorizontalScrollIndicator={false}
+                onScroll={Animated.event(
+                    [{ nativeEvent: { contentOffset: { x: scrollX } } }],
+                    { useNativeDriver: false },
+                )}>
+                {buttons.map(x => (
+                    <View style={[styles.card]} key={x} />
+                ))
+                }
+                
+            </ScrollView>
+            <View style={styles.bottom}>
+              <Text style={styles.bottomText}>Feito por Almir Stark</Text>
+            </View>
         </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
+    );
+}
+
+function ButtonContainer({ buttons, onClick, scrollX }) {
+    const [btnContainerWidth, setWidth] = useState(0);
+    const btnWidth = btnContainerWidth / buttons.length;
+    const translateX = scrollX.interpolate({
+        inputRange: [0, width],
+        outputRange: [0, btnWidth],
+    });
+    const translateXOpposit = scrollX.interpolate({
+        inputRange: [0, width],
+        outputRange: [0, -btnWidth],
+    });
+    return (
+        <View
+            style={styles.btnContainer}
+            onLayout={e => setWidth(e.nativeEvent.layout.width)}>
+            {buttons.map((btn, i) => (
+                <TouchableOpacity
+                    key={btn}
+                    style={styles.btn}
+                    onPress={() => onClick(i)}>
+                    <Text>{btn}</Text>
+                </TouchableOpacity>
+            ))}
+            <Animated.View
+                style={[
+                    styles.animatedBtnContainer,
+                    { width: btnWidth, transform: [{ translateX }] },
+                ]}>
+                {buttons.map(btn => (
+                    <Animated.View
+                        key={btn}
+                        style={[
+                            styles.animatedBtn,
+                            { width: btnWidth, transform: [{ translateX: translateXOpposit }] },
+                        ]}>
+                        <Text style={styles.btnTextActive}>{btn}</Text>
+                    </Animated.View>
+                ))}
+            </Animated.View>
+        </View>
+    );
+}
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
+    container: {
+        flex: 1,
+        paddingVertical: 5
+    },
+    btnContainer: {
+        height: 40,
+        borderRadius: 5,
+        overflow: 'hidden',
+        flexDirection: 'row',
+        backgroundColor: 'gray',
+        width: '100%',
+    },
+    btn: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    animatedBtnContainer: {
+        height: 40,
+        flexDirection: 'row',
+        position: 'absolute',
+        overflow: 'hidden',
+        backgroundColor: '#444',
+    },
+    animatedBtn: {
+        height: 40,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    btnTextActive: {
+        color: '#fff',
+        fontWeight: 'bold',
+    },
+    card: {
+        width: width - 10,
+        height: '100%',
+        marginHorizontal: 5,
+        borderRadius: 5,
+        backgroundColor: '#4B0082',
+    },
+    bottom:{
+      alignItems: "center",
+      display: "flex",  
+      marginTop: 5,
+      marginBottom: -5,
+      backgroundColor: 'black',
 
-export default App;
+    },
+    bottomText:{
+      padding: 10,
+      fontSize: 15,
+      color: "#fff",
+    }
+});
